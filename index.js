87f1a5ac564094f02b86890eefab83535cd54911
Kristian Trenskow <trenskow@me.com>
2020-08-07 12:43:13 +0200

    Now filter common query string parameters in strings.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-module.exports = exports = (type, subtype, payload) => {
+const
+	objectMap = require('@trenskow/object-map');
+
+const transform = (value) => {
+	if (Array.isArray(value)) return value.map(transform);
+	if (value && typeof value === 'object') return objectMap.values(value, (_, value) => transform(value));
+	if (typeof value === 'string') return value.replace(/(\?|&)(auth|app|password|secret)=.*?(&|$)/i, '$1$2=🤫$3');
+	return value;
+};
+
+exports = module.exports = (type, subtype, payload) => {
 	if (!subtype && !payload) {
 		payload = type;
 		type = 'info';
@@ -10,7 +20,7 @@ module.exports = exports = (type, subtype, payload) => {
 		time: new Date(),
 		type: type,
 		subtype: subtype,
-		payload: payload
+		payload: transform(payload)
 	};
 	console.info(JSON.stringify(log, null, process.env.LOG_OUTPUT !== 'pretty' ? undefined : 4));
 	return log;
